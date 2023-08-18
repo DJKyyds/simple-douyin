8c8d1ee4a2eb20dec1d3eed861a94ba8bf928699
“bestow.” <2114275890@qq.com>
2023-08-18 19:36:18 +0800

    完善功能

--- go/config/tencent_cos.go
+++ go/config/tencent_cos.go
@@ -2,7 +2,7 @@ package config
 
 // 替换Id
 
-const SecretId = "AKIDAG8vb5OTN7iBVknegXFoFalniU5Xqzua"
+const SecretId = "xxxxxxxxxxxxxxxxx"
 
 // 替换Key
 
@@ -11,8 +11,6 @@ const SecretKey = "ZCxhN9mDPM5f7MSK1Yt0scsyxnYBf4Nf"
 // 替换你的oss的域名
 
 const CosUrl = "https://lostandfound-1306308561.cos.ap-chengdu.myqcloud.com"
-
-//const CosUrl = "https://cdn.djkyyds.fun"
 
 const ReplaceSuffix = ".jpg"
 
